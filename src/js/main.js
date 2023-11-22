2f570a8c78f36293938bf9ab765ff02196a3706e
Lagutina Svetlana <lagutinasvetlana@hotmail.com>
2023-11-22 13:32:27 +0300

    Added animation fadeIn and fadeOut

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,6 @@ $('div').click(function() {
 
 // console.log($('div').eq(2).find('.more'));
 // console.log($('.some').closest('.findme'));
-console.log($('.more').eq(0).siblings());
+$('button').fadeOut(1800);
 
 // console.log($('button').html('Hello'));
